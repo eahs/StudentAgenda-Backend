f46cdfb8d1230e5a3f0fb31e95dff98fe61d3418
Esteban Martinez <SteveTheHype7@gmail.com>
2020-11-24 10:46:52 -0500

    Added event model

--- ADSBackend/Data/ApplicationDbContext.cs
+++ ADSBackend/Data/ApplicationDbContext.cs
@@ -22,5 +22,7 @@ namespace ADSBackend.Data
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
+
+        public DbSet<ADSBackend.Models.EventInfo> EventInfo { get; set; }
     }
 }
---
+++ ADSBackend/Migrations/20201124153601_AddedEventInfo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ADSBackend.Migrations
+{
+    public partial class AddedEventInfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EventInfo",
+                columns: table => new
+                {
+                    EventId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventInfo", x => x.EventId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventInfo");
+        }
+    }
+}
