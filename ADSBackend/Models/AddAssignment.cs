4b6728fd957fb97e545312a0b97c5b805a89faaf
Esteban Martinez <SteveTheHype7@gmail.com>
2021-01-07 09:38:08 -0500

    Comments added on Add assignment
    To remember for later use, a comment has been added to show that an optional time field needs to be added or a new model needs to be made.

--- ADSBackend/Models/AddAssignment.cs
+++ ADSBackend/Models/AddAssignment.cs
@@ -13,6 +13,8 @@ namespace ADSBackend.Models
         public int Id { get; set; }
 
         //not sure how to make a choice of classes but to be integrated in future version
+        //Need to add field for optional neccesarry time.
+
         public string Class { get; set; }
 
         public string Event { get; set; }
@@ -27,6 +29,7 @@ namespace ADSBackend.Models
 
         [Display(Name = "Estimated Amount of Time Needed (hours)")]
         public double timeNeeded { get; set; }
+
 
         [DataType(DataType.Date)]
 
