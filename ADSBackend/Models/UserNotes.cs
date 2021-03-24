016395dc89d08d8219a4087d81fb9cce83dddfa4
Esteban Martinez <SteveTheHype7@gmail.com>
2021-03-23 21:07:09 -0400

    Added Models for user notes page and about us page
    2 very simple models with controllers. User notes will be for the user to just be able to have a few notes. And The about us page will just be an about us section so the create, delete, etc.  views will be disabled.

---
+++ ADSBackend/Models/UserNotes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADSBackend.Models
+{
+    public class UserNotes
+    {
+        public int UserNotesId { get; set; }
+
+        public string noteName { get; set; }
+
+        public string noteContent { get; set; }
+    }
+}
